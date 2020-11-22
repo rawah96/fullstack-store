60f7f5894b7b181c5b80f76bae1102189868c06c
Goutham <podili@pdx.edu>
2020-11-21 19:59:49 -0800

    added route to get product by id

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -10,4 +10,16 @@ router.route("/").get(
   })
 );
 
+router.route("/:id").get(
+  asyncHandler(async (req, res) => {
+    const product = await Product.findById(req.params.id);
+    if (product) {
+      res.json(product);
+    } else {
+      res.status(404);
+      throw new Error("Product not found");
+    }
+  })
+);
+
 module.exports = router;
