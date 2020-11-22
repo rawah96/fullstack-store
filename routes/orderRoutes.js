0a047f3226b1cfe1aec1928ab25effcd2bbe9ff1
Goutham <podili@pdx.edu>
2020-11-22 12:43:54 -0800

    Added route to get order by ID

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -37,3 +37,18 @@ router.route("/").post(
     }
   })
 );
+
+router.route("/:id").get(
+  asyncHandler(async (req, res) => {
+    const order = await Order.findById(req.params.id).populate(
+      "user",
+      "name email"
+    );
+    if (order) {
+      res.json(order);
+    } else {
+      res.status(404);
+      throw new Error("Order not found");
+    }
+  })
+);
