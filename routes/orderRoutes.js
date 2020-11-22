5f165c261537ae4e6bf4d65959a2156d16b1d1ea
Goutham <podili@pdx.edu>
2020-11-22 11:50:51 -0800

    Added order routes

---
+++ routes/orderRoutes.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const router = express.Router();
+const asyncHandler = require("express-async-handler");
+const Order = require("../models/orderModel");
+
+router.route("/").post(
+  asyncHandler(async (req, res) => {
+    const {
+      orderItems,
+      shippingAddress,
+      paymentMethod,
+      itemsPrice,
+      taxPrice,
+      shippingPrice,
+      totalPrice,
+    } = req.body;
+
+    if (orderItems && orderItems.length === 0) {
+      res.status(400);
+      throw new Error("No order items");
+      return;
+    } else {
+      const order = new Order({
+        orderItems,
+        user: req.user._id,
+        shippingAddress,
+        paymentMethod,
+        itemsPrice,
+        taxPrice,
+        shippingPrice,
+        totalPrice,
+      });
+
+      const createdOrder = await order.save();
+
+      res.status(201).json(createdOrder);
+    }
+  })
+);
