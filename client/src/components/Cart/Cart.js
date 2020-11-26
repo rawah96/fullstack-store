0a171bd3424397c3063c19866d27919bfec18581
rawah <rawah@pdx.edu>
2020-11-25 23:18:29 -0800

    started to implement the checkout page

--- client/src/components/Cart/Cart.js
+++ client/src/components/Cart/Cart.js
@@ -1,9 +1,18 @@
 import React from 'react'
-
+import './Cart.css'
+import Total from './Total'
 function Cart() {
     return (
-        <div>
-            cart
+        <div className="cart">
+                <div className="cart-items">
+                    <h3>Shopping Cart</h3>
+                </div>
+                <div className="cart-total">
+                    <Total />
+                    <button
+                className="checkout-btn"
+                >Checkout</button>
+                </div>
         </div>
     )
 }
---
+++ client/src/components/Cart/Total.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import './Total.css'
+import CurrencyFormat from 'react-currency-format';
+
+function Total({value, items}) {
+    return (
+        <div className="total">
+            <CurrencyFormat 
+                renderText={(value) => (
+                    <>
+                    Number of Items: <strong>{items}</strong>
+                    Total: <strong>{value}</strong>
+                    </>
+                )}
+
+                decimalScale={2}
+                value={value}
+                displayType={"text"}
+                thousandSeparator={true}
+                prefix={"$"}
+            />
+        </div>
+    )
+}
+
+export default Total
