d778a07cd96b2865a1f3d0001fcf10cf30e4e930
rawah <rawah@pdx.edu>
2020-11-27 15:54:10 -0800

    removed unnecessary comments

--- routes/auth.js
+++ routes/auth.js
@@ -89,9 +89,4 @@ const router = express.Router();
 router.post('/signup', signup);
 router.post('/signin', signin);
 
-
-// router.post('/profile', requireSignin, (req, res) => {
-//     res.status(200).json({ user: 'profile' })
-// });
-
 module.exports = router;
