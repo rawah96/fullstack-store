772e6f51befef9ebf6216e8151fd3a1fcbdc4003
Goutham <podili@pdx.edu>
2020-11-27 18:13:58 -0800

    added more constants

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -1,53 +1,51 @@
-import React, {useState} from 'react'
-import './Login.css'
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import "./Login.css";
+import { Link } from "react-router-dom";
 
 function Login() {
-    const [email, setEmail] = useState('');
-    const [pass, setPass] = useState('');
-    const signIn = e => {
-        e.preventDefault();
-        // user auth
-    }
+  const [email, setEmail] = useState("");
+  const [pass, setPass] = useState("");
+  const signIn = (e) => {
+    e.preventDefault();
+    // user auth
+    dispatch(login(email, password));
+  };
 
-    const register = e => {
-        e.preventDefault();
-    }
+  const register = (e) => {
+    e.preventDefault();
+  };
 
-    return (
-        <div className="login">
-            <Link to="/">
-                <div className="login-logo">HOME</div>
-            </Link>
+  return (
+    <div className="login">
+      <Link to="/">
+        <div className="login-logo">HOME</div>
+      </Link>
 
-            <div className="login-container">
-                <h1>Sign In</h1>
-                <form>
-                    <label>Email</label>
-                    <input type="text" 
-                        value={email}
-                        onChange={e => setEmail(e.target.value)}
-                    />
+      <div className="login-container">
+        <h1>Sign In</h1>
+        <form onSubmit={signIn}>
+          <label>Email</label>
+          <input
+            type="text"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
 
-                    <label>Password</label>
-                    <input type="password" 
-                        value ={pass}
-                        onChange={e => setPass(e.target.value)}    
-                    />
+          <label>Password</label>
+          <input
+            type="password"
+            value={pass}
+            onChange={(e) => setPass(e.target.value)}
+          />
 
-                    <div className="login-btns">
-                        <button
-                            type="submit"
-                            onClick={signIn}
-                        >Sign in</button>
-                        <button
-                        >Create Account</button>
-                    </div>
-                </form>
-            </div>
-
-        </div>
-    )
+          <div className="login-btns">
+            <button type="submit">Sign in</button>
+            <button>Create Account</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 }
 
-export default Login
+export default Login;
